44e659c6f28052e0653bdcef6c0c3e2cc9a5805e
IAbrahanArley <abrahanarley96@gmail.com>
2023-05-11 13:53:36 -0300

    alimentando banco

--- src/main/java/com/abeweb/movlist/entities/Game.java
+++ src/main/java/com/abeweb/movlist/entities/Game.java
@@ -24,24 +24,28 @@ public class Game {
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
-	private String plataforms;
+	private String platforms;
 	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game() {
 		
 	}
 
-	public Game(Long id, String title, Integer year, String genre, String plataforms, Double score, String imgUrl,
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.plataforms = plataforms;
+		this.platforms = platforms;
 		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
@@ -81,11 +85,11 @@ public class Game {
 	}
 
 	public String getPlataforms() {
-		return plataforms;
+		return platforms;
 	}
 
 	public void setPlataforms(String plataforms) {
-		this.plataforms = plataforms;
+		this.platforms = plataforms;
 	}
 
 	public Double getScore() {
