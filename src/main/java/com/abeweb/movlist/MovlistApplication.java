b4767bde36e2e685fb799a769a1eaddbe2601171
IAbrahanArley <abrahanarley96@gmail.com>
2023-05-11 12:04:05 -0300

    projeto criado

---
+++ src/main/java/com/abeweb/movlist/MovlistApplication.java
@@ -0,0 +1,13 @@
+package com.abeweb.movlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abeweb/movlist/MovlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abeweb.movlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
