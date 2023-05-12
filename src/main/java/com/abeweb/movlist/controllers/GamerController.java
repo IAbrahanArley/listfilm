4c4cd4a52d6b8c838dad1bd14e91abce141a8647
IAbrahanArley <abrahanarley96@gmail.com>
2023-05-12 17:43:25 -0300

    endpoint de busca de game por id

--- src/main/java/com/abeweb/movlist/controllers/GamerController.java
+++ src/main/java/com/abeweb/movlist/controllers/GamerController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.abeweb.movlist.dto.GameDTO;
 import com.abeweb.movlist.dto.GameMinDTO;
 import com.abeweb.movlist.services.GameService;
 
@@ -17,6 +19,12 @@ public class GamerController {
 	@Autowired
 	private GameService gameService;
 	
+	@GetMapping(value = "/{id}")
+	public GameDTO findById(@PathVariable Long id){
+			GameDTO result = gameService.findById(id);
+			return result;
+	}
+
 	@GetMapping
 	public List<GameMinDTO>  findAll(){
 			List<GameMinDTO> result = gameService.findAll();
--- src/main/java/com/abeweb/movlist/services/GameService.java
+++ src/main/java/com/abeweb/movlist/services/GameService.java
@@ -5,15 +5,24 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.abeweb.movlist.dto.GameDTO;
 import com.abeweb.movlist.dto.GameMinDTO;
 import com.abeweb.movlist.entities.Game;
 import com.abeweb.movlist.repositories.GameRepository;
+
 
 @Service
 public class GameService {
 	
 	@Autowired
 	private GameRepository gameRepository;
+	//################Tratar exceçao depois ######################
+	@org.springframework.transaction.annotation.Transactional (readOnly = true)
+	public GameDTO findById(Long id) {
+		var result = gameRepository.findById(id).get();
+		var dto = new GameDTO(result);
+		return dto;
+	}
 	
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
